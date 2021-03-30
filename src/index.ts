5292f41d90c1a31ccce99a6a6afed9c79dab3282
Jun Huang <junyu_huang@hotmail.com>
2021-03-29 23:31:46 -0700

    try package.json fix for deploy

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,7 @@ app.use(passport.session());
 app.use("/", auth);
 
 // for deployment
-app.use(express.static(path.join(__dirname, '/..client/build')));
+app.use(express.static(path.join(__dirname, '/../client/build')));
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
