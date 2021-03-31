8fb8853742efc9e7eb963e587058ff2ab72b3d82
Jun Huang <junyu_huang@hotmail.com>
2021-03-30 20:40:18 -0700

    update knex connection string

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,15 @@
 import knex from "knex";
 
+// || {
+//   host: process.env.PG_HOST,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   database: process.env.PG_DATABASE,
+// }
+
 const db = knex({
   client: "pg",
-  connection: process.env.DATABASE_URL || {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  },
+  connection: `${process.env.DATABASE_URL}?ssl=true`,
 });
 
 export default db;
